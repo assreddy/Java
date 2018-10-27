472a20fa33aadf6cac6dfcdca0b154f17e237c7a
Sree <ambati.sreenu@gmail.com>
2018-10-26 22:11:06 -0700

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -5,11 +5,14 @@ public class ReverseNumber {
         reverseNumber(input);
     }
 
-    //  Reverse an already initialized number
+     //  Reverse an already initialized number
     public static int reverseNumber(int input){
         boolean isNegative = false;
         if(input < 0){
             isNegative = true;
+        }
+        if(input == 0 && input ==1){
+            return input;
         }
         int revNumber = 0;
         while(input !=0){
